e87390db12660fea938af02ed2a47b97122e6cd4
Trixieee-dev <126485215+Trixieee-dev@users.noreply.github.com>
2023-06-08 10:16:29 +0700

    update terbaru

--- Website/tentang.php
+++ Website/tentang.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="../Pict/PET.png" type="image/jpg">
+  <link rel="icon" href="../Pictures/PET.png" type="image/jpg">
   <title>Tentang</title>
   <!-- link css -->
   <link rel="stylesheet" href="../css/tentang.css">
